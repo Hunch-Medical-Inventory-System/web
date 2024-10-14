a13cbb16a160f002a96a8a7c69f75455ef0567c4
SuperJu5tin <superjuslib@gmail.com>
2024-10-14 10:46:30 -0600

    change demo stuff

--- src/components/Lander.vue
+++ src/components/Lander.vue
@@ -12,8 +12,7 @@
       />
     </v-row>
     <v-row class="text-center justify-center ga-5">
-      <v-btn class="button-styles" href="https://hunch-medical-inventory-system.github.io/front-end/login" size="x-large">Demo Login</v-btn>
-      <v-btn class="button-styles" href="https://hunch-medical-inventory-system.github.io/front-end/profile" size="x-large">Demo Profile</v-btn>
+      <v-btn class="button-styles" href="https://hunch-medical-inventory-system.github.io/front-end" size="x-large">Demo</v-btn>
       <v-btn class="button-styles" size="x-large" to="/proposal">Our Proposal</v-btn>
     </v-row>
   </v-container>
