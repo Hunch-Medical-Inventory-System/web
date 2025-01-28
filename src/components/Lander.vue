76727d1921b9726d0f8f3384680518a51e8757d3
Joe Mama <167790565+masterofsoap@users.noreply.github.com>
2025-01-28 14:35:26 +0000

    Web page back up

--- src/components/Lander.vue
+++ src/components/Lander.vue
@@ -12,7 +12,7 @@
       />
     </v-row>
     <v-row class="text-center justify-center ga-5">
-      <v-btn class="button-styles" href="https://hunch-medical-inventory-system.github.io/front-end" size="x-large">Demo</v-btn>
+      <v-btn class="button-styles" href="https://hunch-medical-inventory-system.github.io/" size="x-large">Demo</v-btn>
       <v-btn class="button-styles" size="x-large" to="/proposal">Our Proposal</v-btn>
     </v-row>
   </v-container>
